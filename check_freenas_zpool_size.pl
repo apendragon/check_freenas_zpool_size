314eeb324388cd7aa3e710a5552b9450c21055ee
stderr <tcazali@gmail.com>
2018-04-15 03:02:08 +0200

    handle snmpv3 session

--- check_freenas_zpool_size.pl
+++ check_freenas_zpool_size.pl
@@ -6,6 +6,8 @@ our $VERSION = '0.01';
 use Net::SNMP qw/:debug :snmp/;
 use Monitoring::Plugin;
 
+#Crypt/Rijndae is required to use AES as privacy protocol with snmpv3
+
 use constant {
   FREENAS_MIB_zpoolDescr => '1.3.6.1.4.1.50536.1.1.1.1.2',
   FREENAS_MIB_zpoolAllocationUnits => '1.3.6.1.4.1.50536.1.1.1.1.3',
@@ -29,7 +31,9 @@ sub getopts {
   my $ng= Monitoring::Plugin->new(
     shortname => "freenas_zpool_size",
     usage => "Usage: %s -H <host> -C <community> -z <zpool> " 
-      . "-w <warning> -c <critical> -t <timeout>",
+      . "-w <warning> -c <critical> -t <timeout> "
+      . "[-u <username> -p <password> -X <passphrase> "
+      . "-a <authentication_type> -x <privacy_protocol>]",
     version => $VERSION,
     url => 'https://github.com/freenas-monitoring-plugins/check_freenas_zpool_size',
     blurb => 'This plugin uses FREENAS-MIB to query zpool size with SNMP',
@@ -41,6 +45,11 @@ sub getopts {
   _get_opt_timeout($ng);
   _get_opt_warning($ng);
   _get_opt_zpool($ng);
+  _get_opt_username($ng);
+  _get_opt_password($ng);
+  _get_opt_passphrase($ng);
+  _get_opt_auth_type($ng);
+  _get_opt_privacy_protocol($ng);
   $ng->getopts;
   $ng;
 }
@@ -50,7 +59,6 @@ sub _get_opt_community($) {
   $ng->add_arg(
     spec => 'community|C=s',
     help => q(SNMP community),
-    required => 1
   );
 }
 
@@ -100,13 +108,65 @@ sub _get_opt_zpool($) {
   );
 }
 
+# get snmpv3 opts
+sub _get_opt_username($) {
+  my $ng = shift;
+  $ng->add_arg(
+    spec => 'username|u=s',
+    help => q(snmpv3 username to use),
+  );
+}
+
+sub _get_opt_password($) {
+  my $ng = shift;
+  $ng->add_arg(
+    spec => 'password|p=s',
+    help => q(snmpv3 password to use),
+  );
+}
+
+sub _get_opt_passphrase($) {
+  my $ng = shift;
+  $ng->add_arg(
+    spec => 'passphrase|X=s',
+    help => q(snmpv3 passphrase to use),
+  );
+}
+
+sub _get_opt_auth_type($) {
+  my $ng = shift;
+  $ng->add_arg(
+    spec => 'authprotocol|a=s',
+    help => "snmpv3 authentication type to use (MD5/SHA)",
+  );
+}
+
+sub _get_opt_privacy_protocol($) {
+  my $ng = shift;
+  $ng->add_arg(
+    spec => 'privprotocol|x=s',
+    help => "snmpv3 privacy protocol to use (AES/DES)",
+  );
+}
+
 sub _snmp_debug {
   my $ng = shift;
+  return DEBUG_ALL if $ng->opts->verbose >= 3;
   return DEBUG_MESSAGE if $ng->opts->verbose >= 3;
   return DEBUG_NONE;
 }
 
-sub _init_snmp {
+sub _switch_snmp_version {
+  my $ng = shift;
+  return 'snmpv3' if defined $ng->opts->username;
+  return 'snmpv3' if defined $ng->opts->password;
+  return 'snmpv3' if defined $ng->opts->passphrase;
+  return 'snmpv3' if defined $ng->opts->auth_type;
+  return 'snmpv3' if defined $ng->opts->privacy_protocol;
+  return 'snmpv2c';
+}
+
+sub _init_snmpv2c {
   my $ng = shift;
   my ($session, $error) = Net::SNMP->session(
     -hostname     => $ng->opts->hostname,
@@ -114,16 +174,46 @@ sub _init_snmp {
     -nonblocking => 1,
     -translate   => [-octetstring => 0],
     -debug       => _snmp_debug($ng),
-    #TODO manage snmp versions
     -version     => 'snmpv2c',
     -timeout     => $ng->opts->timeout,
   );
 
   _die($session, $ng) if (!defined $session);
+  $session;
+}
+
+sub _init_snmpv3 {
+  my $ng = shift;
+  my ($session, $error) = Net::SNMP->session(
+    -hostname     => $ng->opts->hostname,
+    -nonblocking => 1,
+    -translate   => [-octetstring => 0],
+    -debug       => _snmp_debug($ng),
+    -version     => 'snmpv3',
+    -timeout     => $ng->opts->timeout,
+    -username    => $ng->opts->username,
+    -authpassword => $ng->opts->password,
+    -authprotocol => $ng->opts->authprotocol,
+    -privpassword => $ng->opts->passphrase,
+    -privprotocol => lc($ng->opts->privprotocol),
+
+  );
+
+  _die($session, $ng) if (!defined $session);
+  $session;
+}
+
+sub _init_snmp {
+  my $ng = shift;
+  my $snmpv = _switch_snmp_version($ng);
+  $snmpv eq 'snmpv3' ? _init_snmpv3($ng) : _init_snmpv2c($ng);
+}
+
+sub check {
+  my ($ng, $session) = @_;
   _get_zpoolDescr($session, $ng);
   snmp_dispatcher();
 }
-
 # hash used to store collected snmp info.
 # Avoids to pass a lot of args in cascading called functions.
 sub _collected($) {
@@ -291,4 +381,5 @@ sub _check_threshold {
 }
 
 my $ng = getopts();
-_init_snmp($ng);
+my $session = _init_snmp($ng);
+check($ng, $session);
